0e3ed943fa150805f601dd90db74ba5a8a25e9db
zsevic <zsevic95@gmail.com>
2023-07-21 12:43:06 +0200

    Initial page UI

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // eslint-disable-next-line
-const Home = () => (
-  <div className="flex justify-center mt-10 items-center">
-    <h1 className="text-xl md:text-4xl">Hello</h1>
-    <button className="bg-blue-300 p-2 rounded mx-20 hover:bg-blue-600 hover:text-white">
-      Click me!
-    </button>
-  </div>
-);
+const Home = () => {
+  const [folderSelected, setFolderSelected] = useState(false);
+
+  const handleFolderSelect = (): void => {
+    setFolderSelected(true);
+  };
+
+  const handleUpscale = (): void => {
+    alert('Upscaling images...');
+  };
+
+  return (
+    <div className="container mx-auto mt-10 max-w-md">
+      <h1 className="text-3xl font-bold mb-6">Image Boost</h1>
+      <p className="mb-4">
+        Please select a folder containing images to upscale, it shouldn&apos;t
+        contain anything except PNG, JPG, JPEG and WEBP images
+      </p>
+      <div className="flex flex-col mb-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-2"
+          onClick={handleFolderSelect}
+        >
+          Select Folder
+        </button>
+        {folderSelected && (
+          <p className="text-sm text-gray-600 mb-4">
+            Folder selected: /path/to/your/folder
+          </p>
+        )}
+
+        <p className="mb-4">
+          {' '}
+          The upscaled images will be saved as JPG images.
+        </p>
+
+        {folderSelected && (
+          <button
+            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mb-2"
+            onClick={handleUpscale}
+          >
+            Upscale Images
+          </button>
+        )}
+        {folderSelected && (
+          <p className="text-sm text-gray-600 mb-4">
+            Folder with upscaled images: /path/to/your/folder_upscaled
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
