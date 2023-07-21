039b3c2ac58118d7451fddfc06e63e6dc24fca41
zsevic <zsevic95@gmail.com>
2023-07-21 11:35:31 +0200

    Configure Tailwind

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 // eslint-disable-next-line
-const Home = () => <p>hello world</p>;
+const Home = () => (
+  <div className="flex justify-center mt-10 items-center">
+    <h1 className="text-xl md:text-4xl">Hello</h1>
+    <button className="bg-red-300 p-2 rounded mx-20 hover:bg-red-600 hover:text-white">
+      Click me!
+    </button>
+  </div>
+);
 
 export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./renderer/pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
