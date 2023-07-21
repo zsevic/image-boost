833e403c796cd749b92c7df07bd2f7c69cec968f
zsevic <zsevic95@gmail.com>
2023-07-21 11:38:29 +0200

    Update button color

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => (
   <div className="flex justify-center mt-10 items-center">
     <h1 className="text-xl md:text-4xl">Hello</h1>
-    <button className="bg-red-300 p-2 rounded mx-20 hover:bg-red-600 hover:text-white">
+    <button className="bg-blue-300 p-2 rounded mx-20 hover:bg-blue-600 hover:text-white">
       Click me!
     </button>
   </div>
