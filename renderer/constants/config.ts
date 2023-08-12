e18ffbb3c95bb18568d75fc9af9d60e733acf5bf
zsevic <zsevic95@gmail.com>
2023-08-12 14:47:33 +0200

    Update API URL

--- renderer/constants/config.ts
+++ renderer/constants/config.ts
@@ -4,6 +4,6 @@ import { isEnvironment } from "../utils";
 export const API_BASE_URL =
   isEnvironment(Environment.Development)
     ? 'http://localhost:8080'
-    : 'https://image-boost-server.fly.dev';
+    : 'https://image-boost-a59b456c29ca.herokuapp.com';
 
 export const ANALYTICS_ID = 'G-C5YB9TYDVP';
