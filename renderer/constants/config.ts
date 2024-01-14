989f10aca2a80fe79ca7bbec791f262d1a07f6a4
zsevic <zsevic95@gmail.com>
2024-01-14 19:51:34 +0100

    Remove unused code

--- renderer/constants/config.ts
+++ renderer/constants/config.ts
@@ -1,8 +1,1 @@
-import { Environment } from '../common/enums/environment';
-import { isEnvironment } from '../utils';
-
-export const API_BASE_URL = isEnvironment(Environment.Development)
-  ? 'http://localhost:8080'
-  : 'https://image-boost-a59b456c29ca.herokuapp.com';
-
 export const ANALYTICS_ID = 'G-C5YB9TYDVP';
--- renderer/utils/request.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { API_BASE_URL } from '../constants/config';
-
-axios.defaults.baseURL = API_BASE_URL;
-
-export default axios;
